924f4dfd2f14a0bafeab128f67d2415b6a9c3deb
Diyalad-2501 <laddiya2007@gmail.com>
2026-04-13 12:56:48 +0530

    Add comments to express4.js for clarification
    Added comments for clarification and credit.

--- T2/Lec1/express4.js
+++ T2/Lec1/express4.js
@@ -1,14 +1,2 @@
-var expr = require('express') 
-var app = expr()
-
-// app.get('/name/:name/age/:age' , (req,res)=>{
-//     res.send(req.params)
-// })
-
-app.get('/:name/:age' , (req,res)=>{
-    res.send(req.params)
-})
-
-app.listen(5008 , ()=>{
-    console.log('Connected')
-})
+// Hiii Diya!
+// Idea given by Jatin (BABA) and executed by Yash kshtariya
