7f40bcc60e86f8b458745beca53423a64082f8f2
Diyalad-2501 <laddiya2007@gmail.com>
2026-04-13 12:55:47 +0530

    Add comments to express2.js
    Added comments to express2.js for clarity.

--- T2/Lec1/express2.js
+++ T2/Lec1/express2.js
@@ -1,17 +1,2 @@
-var expr = require('express')
-var app = expr()
-data = {name :'xyz' , age:24}
-
-app.get('/',(req , res)=>{
-    // res.set('Content-Type' , 'text/html') // or res.type('text/html')
-    // res.set('Content-Type' , 'application/json')
-    // res.write(JSON.stringify(data)) 
-    // res.send(data) set mime type to json
-    // res.json(data)
-
-    res.write(data.age+" ") 
-    res.send()
-})
-app.listen(5006 , ()=>{
-    console.log('Server connected')
-})
+// Hiii Diya!
+// Idea given by Jatin (BABA) and executed by Yash kshtariya
